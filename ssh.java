5ef01d8880ae6c1e398ba69733f98f9f692fa0b7
carmichlaelc09 <127217021+carmichlaelc09@users.noreply.github.com>
2023-03-11 20:22:47 -0500

    updated words

--- ssh.java
+++ ssh.java
@@ -1,1 +1,6 @@
-something elsE
+You're right! "Never give up" is a great attitude to have, especially when facing challenges or pursuing goals. 
+Persistence and perseverance are key qualities that can help us overcome obstacles, learn from failures, and ultimately achieve success.
+However, it's also important to remember that sometimes giving up on a certain approach or goal can be the best decision for our mental health, 
+well-being, and overall success. Sometimes we need to pivot, adapt, or move on to something else that better aligns with our values, passions, or abilities.
+
+In the end, it's all about finding a balance between determination and flexibility, and being willing to learn and grow from our experiences along the way.
